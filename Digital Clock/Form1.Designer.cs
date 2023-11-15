b103f5f93883dd70cd8178bcb72ded1126eddbab
Scott Nixon <tsnixon@sbcglobal.net>
2023-11-15 16:20:21 -0600

    First version

---
+++ Digital Clock/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Digital_Clock
+{
+    partial class frmDigitalClock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Timer timer;
+            this.lblDigitalClock = new System.Windows.Forms.Label();
+            timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblDigitalClock
+            // 
+            this.lblDigitalClock.AutoSize = true;
+            this.lblDigitalClock.Font = new System.Drawing.Font("Courier New", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDigitalClock.Location = new System.Drawing.Point(12, 9);
+            this.lblDigitalClock.Name = "lblDigitalClock";
+            this.lblDigitalClock.Size = new System.Drawing.Size(109, 30);
+            this.lblDigitalClock.TabIndex = 0;
+            this.lblDigitalClock.Text = "label1";
+            // 
+            // timer
+            // 
+            timer.Enabled = true;
+            timer.Interval = 1000;
+            timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // frmDigitalClock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(198, 42);
+            this.ControlBox = false;
+            this.Controls.Add(this.lblDigitalClock);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmDigitalClock";
+            this.Text = "Digital Clock";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDigitalClock;
+    }
+}
+
---
+++ Digital Clock/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+namespace Digital_Clock
+{
+    public partial class frmDigitalClock : Form
+    {
+        public frmDigitalClock()
+        {
+            InitializeComponent();
+        }
+
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            lblDigitalClock.Text = DateTime.Now.ToString("h:mm:ss tt");
+        }
+    }
+}
